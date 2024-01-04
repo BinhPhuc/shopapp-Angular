bc8e53daf373a27aeb1f22b2a083c5a06dd98418
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-01-05 00:37:28 +0700

    in progress...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +29,8 @@ import { HomeComponent } from './home/home.component';
   providers: [
     provideClientHydration()
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+]
 })
 export class AppModule { }
